75379f700a0910a89693d75c2e54d1cb5fb58bee
LukeTHadley <lukehadley2001@gmail.com>
2022-02-06 21:42:18 +0000

    Push of working example.
    Mostely used this video for reference to get this project to work <https://www.youtube.com/watch?v=BZt74PVb7sM>.
    This video also covers the same stuff, but could not get this way working <https://www.youtube.com/watch?v=jbGgVDa_2q8&t>

---
+++ XR-UI_Interaction_Test/Interaction Test/Assets/ButtonPress.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonPress : MonoBehaviour
+{
+
+    public void TestButtonPress(){
+        Debug.Log("Button was pressed");
+    }
+
+}
